7ca83f8493f8dc7ee4131dbf18fcaf958928d7d3
hiroshi Matoba <matobahiroshi@gmail.com>
2015-11-16 14:44:25 +0100

    update voronoi

--- wave_voronoi_1/src/cApp.cpp
+++ wave_voronoi_1/src/cApp.cpp
@@ -48,18 +48,21 @@ class cApp : public AppNative {
     const int win_w = 4320;
     const int win_h = 1920;
 
-    Perlin mPln;
+    vector<Perlin> mPlns;
     vector<Wave> mWave;
-    MayaCamUI mayaCam;
     vector<vPoint> vPs;
     voronoi_diagram<double> vD;
     Exporter mExp;
+    fs::path assetDir;
   
-    int frame       = 185-1;
-    int mFpb        = 1024*4;
+    int frame       = -1;
+    int mFpb        = 1920*2;   //96kHz/25fps = 3840
     float camDist   = -2800;
-    float amp       = 400;
-    int resolution  = 3;
+    float amp       = 600;
+    int resolution  = 2;
+    int totalMovFrame;
+    int xoffset = 120;
+
 };
 
 void cApp::setup(){
@@ -69,23 +72,30 @@ void cApp::setup(){
     setWindowPos( 0, 0 );
     gl::enableVerticalSync();
     
-    mPln.setOctaves(4);
-    mPln.setSeed(444);
+//    CameraPersp persp;
+//    persp.lookAt( Vec3f(0,0, camDist), Vec3f(0,0,0), Vec3f(0,-1,0) );
+//    persp.setAspectRatio( (double)win_w/win_h );
+//    persp.setNearClip(1);
+//    persp.setFarClip(100000);
+//    mayaCam.setCurrentCam( persp );
     
-    CameraPersp persp;
-    persp.lookAt( Vec3f(0,0, camDist), Vec3f(0,0,0), Vec3f(0,-1,0) );
-    persp.setAspectRatio( (double)win_w/win_h );
-    persp.setNearClip(1);
-    persp.setFarClip(100000);
-    mayaCam.setCurrentCam( persp );
-    
+    assetDir = mt::getAssetPath();
     
     for( int w=0; w<8; w++ ){
         mWave.push_back( Wave() );
-        mWave[w].create( "snd/samples/192k/3s1e_192k_" + toString(w+1) + ".wav");
-    }
+        //mWave[w].create( "snd/samples/192k/3s1e_192k_" + toString(w+1) + ".wav");
+        string filename = "n5_2mix_sepf_0" + to_string(w+1)+ ".aif";
+        mWave[w].create( (assetDir/"snd"/"timeline"/"sepf"/filename) );
     
-    mExp.setup( win_w, win_h, 1000, GL_RGB, mt::getRenderPath(), 0 );
+        mPlns.push_back(Perlin());
+        mPlns[mPlns.size()-1].setOctaves(1+w%4);
+        mPlns[mPlns.size()-1].setSeed(123*w*w);
+    }
+
+    int nFrame = mWave[0].buf->getNumFrames();
+    totalMovFrame = nFrame / mFpb;
+    
+    mExp.setup( win_w, win_h, totalMovFrame, GL_RGB, mt::getRenderPath(), 0 );
     
 #ifdef RENDER
     mExp.startRender();
@@ -108,18 +118,48 @@ void cApp::update(){
     
 
     vPs.clear();
+    
+    float canvasWidth = mExp.mFbo.getWidth()-xoffset*2;
+    float sampleDistance = (float)canvasWidth/mFpb;
+    
     for( int w=0; w<mWave.size(); w++ ){
         const float * ch0 = mWave[w].buf->getChannel( 0 );
+        //const float * ch1 = mWave[w].buf->getChannel( 1 );
         
         for ( int i=0; i<mFpb; i++) {            
             if( i!=1 && i%resolution==0 ){
                 float l = ch0[audioPos + i];
-                float x = i - mFpb/2 + w*0.5;
-                float y = l * amp;
-                Vec2f n = mPln.dfBm( getElapsedFrames()*0.1 + i*0.1, 1+l*0.9);
-                x += n.x*2;
-                y += n.y*0.1;
-                vPs.push_back( vPoint(x, y) );
+                //float r = ch1[audioPos + i];
+                
+                float x1 = i * mExp.mFbo.getWidth()/canvasWidth;
+                x1 -= mExp.mFbo.getWidth()/2;
+                x1 += xoffset;
+                
+                x1 += sampleDistance * w/mWave.size();
+                //float x2 = i - mFpb/2 + sampleDistance*0.5;
+                
+                float y1 = l * amp;
+                y1 *= (w+2)*0.4;
+
+                //float y2 = r * amp - 10;
+                Vec3f n1 = mPlns[w].dfBm( frame*0.1*w, w*i*0.2, 1+l*0.5);
+                //Vec2f n2 = mPln.dfBm( getElapsedFrames()*10 + i*3, 1+r*0.91);
+                x1 += n1.x*n1.z*8;
+                y1 += n1.y*n1.x*8;
+                
+                float yoffset = 16;
+                y1 -= yoffset*(mWave.size()-1)/2;
+                y1 += w * yoffset;
+                
+                //x2 += n2.x*2;
+                //y2 += n2.y*2;
+
+                if( mPlns[w].noise(x1*0.1, y1 ) > 0.95  ){
+                    y1 *= randFloat(2,6);
+                }
+
+                vPs.push_back( vPoint(x1, y1) );
+                //vPs.push_back( vPoint(x2, y2) );
             }
         }
     }
@@ -132,14 +172,28 @@ void cApp::draw(){
 
     gl::enableAlphaBlending();
     
-    mExp.begin( mayaCam.getCamera() ); {
+    mExp.begin(); {
         gl::clear( Color(0,0,0) );
+        glPushMatrix();
+        gl::translate(mExp.mFbo.getWidth()/2, mExp.mFbo.getHeight()/2);
         draw_voronoi();
+        glPopMatrix();
+        
+        //if( !mExp.bRender && !mExp.bSnap ){
+            glPointSize(4);
+            gl::color(1, 0, 0, 1);
+            //glBegin(GL_POINTS);
+            glBegin(GL_LINES);
+            glVertex3f( 120, win_h/2, 0 );
+            glVertex3f( win_w-120, win_h/2, 0 );
+            glEnd();
+        //}
     } mExp.end();
 
     gl::clear( Color(0,0,0) );
     glColor3f(1, 1, 1);
     mExp.draw();
+    
 }
 
 void cApp::draw_voronoi(){
@@ -172,19 +226,19 @@ void cApp::draw_voronoi(){
                                 bool draw = !xOver && !yOver;
                                 
                                 if(draw){
-                                    gl::color(0.7, 0.7, 0.7, 0.7);
+                                    gl::color(0.7, 0.7, 0.7, 0.5);
                                     glVertex3f(x0, y0, 0);
                                     glVertex3f(x1, y1, 0);
                                 }else{
                                     if( randFloat()>0.9){
-                                        gl::color(0.5, 0.5, 0.5);
+                                        gl::color(0.5, 0.5, 0.5, 0.5);
                                         glVertex3f(x0, y0, 0);
                                         glVertex3f(x1, y1, 0);
                                     }
                                 }
                             }
                         }else{
-                            if( 1 ){
+                            if( 0 ){
                                 const vertex_type * v0 = edge->vertex0();
                                 if( v0 ){
                                     vPoint p1 = vPs[edge->cell()->source_index()];
@@ -208,13 +262,15 @@ void cApp::draw_voronoi(){
     
     // draw Point
     if( 0 ){
-        glPointSize(2);
+        glPointSize(3);
         gl::color(1.0, 0.1, 0.1, 0.7);
         glBegin(GL_POINTS);
         for( auto v : vPs )
             glVertex3f( v.x(), v.y(), 0);
         glEnd();
     }
+    
+
 }
 
 

cc10821aa3e1e3694c13c7d75952e0a587f7ee83
stdmtb <matobahiroshi@gmail.com>
2016-01-12 16:05:38 +0100

    perlin  red particle

--- red_particle_2_tbb/src/cApp.cpp
+++ red_particle_2_tbb/src/cApp.cpp
@@ -1,4 +1,4 @@
-#define RENDER
+//#define RENDER
 #define PROXY
 
 #include "cinder/app/AppNative.h"
@@ -23,7 +23,7 @@ class cApp : public AppNative {
 	void setup();
     void update();
     void draw();
-    void task(float i, float frame);
+    void task(float i );
     
     void keyDown( KeyEvent event );
     void mouseMove( MouseEvent event );
@@ -32,104 +32,108 @@ class cApp : public AppNative {
     void resize();
     
     Exporter mExp;
-    int mW = 1080*4;
-    int mH = 1920;
+    int mW;
+    int mH;
     
     VboSet vbo;
     vector<Vec3f> spd;
     vector<Vec3f> dest;
 
     Perlin mPln;
+    Perlin mPln2;
+
     int totalSize = mW * mH;
     Vec3f center = Vec3f(mW/2, mH/2, 0);
     int frame = 0;
+    
+    vector<Vec2i> absPos = { Vec2i(1640,192), Vec2i(2680,192), Vec2i(1640,1728), Vec2i(2680,1728) };
 };
 
 void cApp::setup(){
+
 #ifdef PROXY
-    mW *= 0.5;
-    mH *= 0.5;
+    mW = 1000;
+    mH = 1000;
+#else
+    mW = 1080*4;
+    mH = 1920;
 #endif
-    
     setWindowSize( mW, mH );
     mExp.setup( mW, mH, 0, 1000-1, GL_RGB, mt::getRenderPath(), 0);
     setWindowPos( 0, 0 );
     mPln.setSeed(13251);
     mPln.setOctaves(4);
 
-    for (int i=0; i<2; i++) {
-            for (int y=0; y<mH; y++) {
-                for (int x=0; x<mW; x++) {
-                    Vec3f vel = mPln.dfBm(x*0.005, y*0.005, i*0.01)*2;
-                    vbo.addPos( Vec3f(x, y, 0) );
-                    vbo.addCol( ColorAf(1,0,0,0.5) );
-                    spd.push_back( Vec3f(vel.x, vel.y+vel.z*1.5, 0) );
-                    
-                    dest.push_back( Vec3f(x, y, 0) );
-                }
+    mPln2.setSeed(29131);
+    mPln2.setOctaves(4);
+
+    for (int y=0; y<mH; y+=2) {
+        for (int x=0; x<mW; x+=2) {
+            Vec2f vel = mPln.dfBm(x*0.005, y*0.005 )*2;
+            vbo.addPos( Vec3f(randFloat()*mW, randFloat()*mH, 0) );
+            //vbo.addPos( Vec3f(x, y, 0) );
+            vbo.addCol( ColorAf(1,0,0,1) );
+            spd.push_back( Vec3f(vel.x, vel.y, 0) );
+            dest.push_back( Vec3f(200, 200, 0) );
         }
     }
-    
     vbo.init( GL_POINTS, true, true );
-    
+
 #ifdef RENDER
     mExp.startRender();
 #endif
 }
 
-void cApp::task( float i, float frame ){
-
-    Vec3f p = vbo.getPos()[i];
-    Vec3f n = mPln.dfBm( p.x*0.005f, p.y*0.005f, frame*0.1f );
-    n.x = n.x * n.z;
-    n.y = n.y * n.z;
-
-    int life = getElapsedFrames() - (int)i/10000;
-
-    if( life<=400){
-        spd[i].x += pow(n.x, 3)*3.0 * n.z;
-        spd[i].y += pow(n.y, 3)*3.0 * n.z;
-
-    }else{
-        float rate = 1.0f/ (1+(life-400)*0.01);
-        rate = MAX(rate, 0.1);
-        spd[i].x += (n.x * rate);
-        spd[i].y += (n.y * rate);
-//        spd[i].z += (n.z * rate);
-
-    }
-    
-    p.x += spd[i].x;
-    p.y += spd[i].y;
-    // p.z += spd[i].z;
-    
-    vbo.writePos(i, p);
-    
-    if( life > 400 ){
-                Vec3f dir = dest[i] - p;
-        dir = dir.normalized();
-        
-        float len = spd[i].length();
-        spd[i] = spd[i].normalized()*0.9 + dir*0.1;
-        spd[i] *= len;
-    }else{
-    
-    }
-}
-
 void cApp::update(){
 
-    int loadNum = frame/400.0f * vbo.getPos().size();
-    loadNum = MIN( vbo.getPos().size(), loadNum );
-    
+    int loadNum = vbo.getPos().size();
+                  
     parallel_for( blocked_range<size_t>(0, loadNum), [=](const blocked_range<size_t>& r) {
         for( size_t i=r.begin(); i!=r.end(); ++i ){
-            task( i, frame );
+            task( i );
         }
     });
     
     vbo.updateVboPos();
+}
+
+
+void cApp::task( float i  ){
+
+    //int life = getElapsedFrames() - (int)i/300;
+    Vec3f p = vbo.getPos()[i];
+    Vec3f n = mPln.dfBm( p.x*0.001f, p.y*0.001f, (frame)*0.01f ) * 0.2f;
     
+    spd[i].x += n.x*n.z;
+    spd[i].y += n.y*n.z;
+    
+    p.x += spd[i].x;
+    p.y += spd[i].y;
+    
+    if( frame > 200 ){
+        Vec3f dir = dest[i] - p;
+        double dist = dir.length();
+        
+        dir.normalize();
+        Vec3f n2 = mPln2.dfBm( p.x*0.0001, p.y*0.0001 ,frame*0.01 )*0.1;
+        dir += n2;
+        dir.normalize();
+        
+        float len = spd[i].length();
+        len = MAX(0.5, len);
+        spd[i] = spd[i].normalized()*0.95 + dir*0.05;
+        spd[i] *= len;
+        
+        if( dist < 6 ){
+            ColorAf c = vbo.getCol()[i];
+            c.a = 0;
+            vbo.writeCol(i, c);
+        }
+            
+    }
+    
+    vbo.writePos(i, p);
+
 }
 
 void cApp::draw(){

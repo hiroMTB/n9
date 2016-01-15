5b9c3a31d41e4014f55056635743dfc4beb2ef5a
stdmtb <matobahiroshi@gmail.com>
2016-01-15 14:30:46 +0100

    red_particle up

--- red_particle_4/src/cApp.cpp
+++ red_particle_4/src/cApp.cpp
@@ -66,9 +66,11 @@ class cApp : public AppNative {
     vector<Perlin> mPln;
     vector<Vec2i> absPos = { Vec2i(1640,192), Vec2i(2680,192), Vec2i(1640,1728), Vec2i(2680,1728) };
 
-    vector<Particle> ptcl;
-    VboSet vbo;
+    vector<vector<Particle>> ptcls;
+    vector<VboSet> vbos;
     Exporter mExp;
+
+    vector<vector<Colorf>> picker;
     
 };
 
@@ -80,7 +82,14 @@ void cApp::setup(){
     setWindowPos( 0, 0 );
     mExp.setup( mW, mH, 0, 1000-1, GL_RGB, mt::getRenderPath(), 0);
     
-    mPln.push_back( Perlin(4, 5555) );
+
+    for( int i=0; i<4; i++){
+        mPln.push_back( Perlin(4, mt::getSeed() + 111*i) );
+        ptcls.push_back( vector<Particle>() );
+        vbos.push_back( VboSet() );
+    }
+    
+    mt::loadColorSample( "img/colorSample/n5pickCol_0.png", picker );
     
 #ifdef RENDER
     mExp.startRender();
@@ -96,74 +105,133 @@ void cApp::update(){
 
 void cApp::add_particle(){
 
-    int nAdd = 3000 + 1000 * (mPln[0].fBm( 1+frame*0.01 )+1.0);
-    for( int i=0; i<nAdd; i++){
+    int np = -1;
+    for( auto & ptcl : ptcls ){
         
-        Particle pt;
-        pt.life = randInt(300, 600);
-        pt.id = ptclCount++;
-        pt.mass = randFloat(0.5, 1.1);
-        pt.pos.set(absPos[0].x, absPos[0].y, 0);
+        np++;
+        int nAdd = 3000;
+        for( int i=0; i<nAdd; i++){
+            
+            Particle pt;
+            pt.life = randInt(100, 200);
+            pt.id = ptclCount++;
+            pt.mass = randFloat(0.5, 1.1);
+            pt.pos.set( absPos[0].x, absPos[0].y, 0);
+            
+            //        Vec3f v( randFloat(-1,1),randFloat(-1,1),randFloat(-1,1) );
+            float noiseScale = 0.002;
+            Vec3f v = mPln[np].dfBm( pt.id*noiseScale, i+noiseScale, 10+pt.mass+0.02 );
+            pt.vel = v*30;
+            
+            
+            ptcl.push_back( pt );
+        }
         
-        Vec3f v = mPln[0].dfBm( pt.id,  randFloat(-1,i)*0.1, randFloat(-i,1)*0.1 ) * 3.0;
-        Vec3f f = mPln[0].dfBm( pt.id, randFloat(-1,i)*0.1,  randFloat(-i,1)*0.1 ) * 3.0;
-        
-        pt.vel = v;
-        pt.force = f;
-        ptcl.push_back( pt );
     }
 }
 
 void cApp::update_particle(){
     
-    int loadNum = ptcl.size();
-    int substep = 1;
-    
-    for( int j=0; j<substep; j++){
-        parallel_for( blocked_range<size_t>(0, loadNum), [=](const blocked_range<size_t>& r) {
-            for( size_t i=r.begin(); i!=r.end(); ++i ){
-                Particle & pt = ptcl[i];
-                Vec3f & p = pt.pos;
-                Vec3f & v = pt.vel;
-                Vec3f & f = pt.force;
-                
-                Vec3f n = mPln[0].dfBm( p.x*0.01 + randFloat(-1,1)*0.0001, p.y*0.01+ randFloat(-1,1)*0.0001, frame*0.02 ) * 1.0;
-
-                //n.x *= abs(n.z+1.0);
-                //n.y *= abs(n.z+1.0);
-                //f = f*0.5 + n*0.5;
-
-                n.x = pow(n.x, 3) * 5 * abs(n.z+1.0);
-                n.y = pow(n.y, 3) * 5 * abs(n.z+1.0);
-                n.rotateZ( (i+frame)*0.00001 );
-                
-                f = f*0.5 + n*0.5;
-                
-                v = v*0.99 + f;
-                p += v;
-                p.z = 0;
-                pt.life--;
-            }
-        });
+    int np = -1;
+    for( auto & ptcl : ptcls ){
+        
+        np++;
+        int loadNum = ptcl.size();
+        int substep = 1;
+        
+        int imgw = picker.size();
+        int imgh = picker[0].size();
+        
+        for( int j=0; j<substep; j++){
+            parallel_for( blocked_range<size_t>(0, loadNum), [&](const blocked_range<size_t>& r) {
+                for( size_t i=r.begin(); i!=r.end(); ++i ){
+                    Particle & pt = ptcl[i];
+                    Vec3f & p = pt.pos;
+                    Vec3f & v = pt.vel;
+                    Vec3f & f = pt.force;
+                    
+                    Vec3f dir_n = p.safeNormalized();
+                    dir_n.z = 0;
+                    pt.life--;
+                    
+                    if( 0 ){
+                        
+                        int idx = (int)abs(p.x)%imgw;
+                        int idy = (int)abs(p.y)%imgh;
+                        Colorf & c = picker[idx][idy];
+                        
+                        Vec3f n = mPln[np].dfBm( c.r*0.001, c.g*0.001, c.b*0.001 + frame*0.001);
+                        n *= 2.0;
+                        f = n;
+                        v = v*0.5 + f*0.5;
+                        p += v;
+                        p.z = 0;
+                    }else{
+                        
+                        //
+                        // Perlin base
+                        //
+                        Vec2f p2d(p.x, p.z);
+                        float dist = (p2d-absPos[0]).length();
+                        //float noiseScale = lmap(dist, 0.0f, 4320.0f*0.1f, 0.02f, 0.002f);
+                        
+                        float noiseScale = 0.002 - dist*0.00001;
+                        
+                        float amp = 15;
+                        
+                        float ox = 10;
+                        float oy = 10;
+                        float oz = 10;
+                        
+                        Vec3f n = mPln[np].dfBm( ox+p.x*noiseScale, oy+p.y*noiseScale, oz+frame*(0.003 - dist*0.000001) );
+                        Vec2f n2d(n.x, n.y);
+                        if( n2d.length()<0.4)
+                            n = n.safeNormalized()*0.4;
+                        
+//                        n.x += randFloat(-1.0f,1.0f) * 0.1;
+//                        n.y += randFloat(-1.0f,1.0f) * 0.1;
+//                        n.z += randFloat(-1.0f,1.0f) * 0.1;
+                        
+                        f = f*0.5 + n*amp * 0.5;
+                        v = v*0.5 + f*0.5;
+                        
+                        float vlen = v.length();
+                        vlen = MAX(vlen, 1.0);
+                        
+                        v = (v.normalized()+dir_n).normalized() * vlen;
+                        
+                        p += v;
+                        p.z = 0;
+                        
+                    }
+                }
+            });
+        }
     }
 }
 
 void cApp::update_vbo(){
 
-    vbo.resetVbo();
+    int nv = -1;
+    for( auto & vbo : vbos ){
+        nv++;
+        vbo.resetVbo();
     
-    for (int i=0; i<ptcl.size(); i++) {
-        if( ptcl[i].life > 0){
-            vbo.addPos( ptcl[i].pos );
-            vbo.addCol( ColorAf(1,0,0,1) );
+        vector<Particle> & ptcl = ptcls[nv];
+        
+        for (int i=0; i<ptcl.size(); i++) {
+            if( ptcl[i].life > 0){
+                vbo.addPos( ptcl[i].pos );
+                vbo.addCol( ColorAf(1,0,0,0.8) );
+            }
         }
-    }
-    
-    if(vbo.vbo){
-        vbo.updateVboPos();
-        vbo.updateVboCol();
-    }else{
-        vbo.init( GL_POINTS );
+        
+        if(vbo.vbo){
+            vbo.updateVboPos();
+            vbo.updateVboCol();
+        }else{
+            vbo.init( GL_POINTS );
+        }
     }
 }
 
@@ -173,10 +241,14 @@ void cApp::draw(){
 
         gl::enableAlphaBlending();
         
-        glPointSize(2);
+        glPointSize(1);
         gl::clear( Colorf(0,0,0) );
         gl::pushMatrices();
-        vbo.draw();
+//        glTranslatef( absPos[0].x, absPos[0].y, 0);
+        
+        for( auto & vbo : vbos ){
+            vbo.draw();
+        }
 
         gl::popMatrices();
 
@@ -184,16 +256,12 @@ void cApp::draw(){
     
     mExp.draw();
     
-    gl::pushMatrices();
-    glTranslatef( mW/2, mH/2, 0);
-    mt::drawCoordinate();
     mt::drawScreenGuide();
-    gl::popMatrices();
     
     gl::color(0, 0, 0);
     gl::drawString( "fps : "+to_string(getAverageFps()) , Vec2i(20,20));
     gl::drawString( "frame : "+to_string(getElapsedFrames()) , Vec2i(20,40));
-    gl::drawString( "nParticle : "+to_string(vbo.getPos().size()) , Vec2i(20,60));
+   // gl::drawString( "nParticle : "+to_string(vbo.getPos().size()) , Vec2i(20,60));
     
     frame++;
 }
